df5dcf241fc091bb4fc4e9a33598a42add78de25
ShinDong <sdh0521@gmail.com>
2023-10-26 11:30:12 +0900

    웹 크롤링 연습2

--- web1.py
+++ web1.py
@@ -5,4 +5,26 @@ from bs4 import BeautifulSoup
 page=open("c:\\work\\test01.html", "rt", encoding="utf-8").read()
 
 soup=BeautifulSoup(page, "html.parser")
-print(soup.prettify())
+
+# 전체 보기
+# print(soup.prettify())
+
+# <p>태그 전체를 검색
+# print(soup.find_all("p"))
+
+# 첫번째 <p>태그만 검색
+# print(soup.find("p"))
+
+# <p class="outer-text"> 형태만 검색
+# print(soup.find_all("p", class_="outer-text"))
+# attrs를 사용(위 대신 이렇게 사용)
+# print(soup.find_all("p", attrs={"class":"outer-text"}))
+
+# 특정 태그만 지정할 경우 id속성
+# print(soup.find_all(id="first"))
+
+# 태그 내부의 컨텐츠를 가져오기
+for tag in soup.find_all("p"):
+    title=tag.text.strip()
+    title=title.replace("\n", "").replace("\t","")
+    print(title)
---
+++ web2.py
@@ -0,0 +1,43 @@
+# web2.py
+
+import requests # 웹서버 요청
+from bs4 import BeautifulSoup  # 크롤링
+
+url="https://www.daangn.com/fleamarket/"
+response=requests.get(url)
+soup=BeautifulSoup(response.text, "html.parser")
+
+f=open("c:\\work\\dangn.txt", "wt", encoding="utf-8")
+# <div class 속성 딕셔너리>
+posts=soup.find_all("div", attrs={"class":"card-desc"})
+for post in posts:
+    title=post.find("h2", attrs={"class":"card-title"})
+    price=post.find("div", attrs={"class":"card-price"})
+    addr=post.find("div", attrs={"class":"card-region-name"})
+    
+    title=title.text.strip().replace("\n", "")
+    price=price.text.strip().replace("\n", "")
+    addr=addr.text.strip().replace("\n", "")
+
+    print("물건: {0} / 가격: {1} / 주소: {2}".format(title, price, addr))
+    # f를 붙이고 변수명 넘기기
+    f.write(f"물건: {title} / 가격: {price} / 주소: {addr}\n")
+
+# <div class="card-desc">
+#       <h2 class="card-title">다이슨청소기</h2>
+#       <div class="card-price ">
+#         50,000원
+#       </div>
+#       <div class="card-region-name">
+#         대구 수성구 범어2동
+#       </div>
+#       <div class="card-counts">
+#           <span>
+#             관심 28
+#           </span>
+#         ∙
+#         <span>
+#             채팅 26
+#           </span>
+#       </div>
+#     </div>
